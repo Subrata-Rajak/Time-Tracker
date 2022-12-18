84e2c6edcdfa63cd6fd5700f5a8a021b375718e6
Subrata Rajak <subrata324cse@gmail.com>
2022-12-18 17:36:19 +0530

    first day

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          ElevatedButton(
+            child: const Text('Sign In With Google'),
+            onPressed: () {
+              print("Button Pressed");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
